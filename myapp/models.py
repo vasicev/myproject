65c90c98c8f213d149d193407b7faaee726b75c0
vasicev <vasicevdeki@gmail.com>
2021-05-03 11:00:41 +0200

    task2

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tag = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.title
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import home
 
 urlpatterns = [
-    path('hello-world/', home)
+    path('task1/', home)
 ]
