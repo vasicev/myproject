dd84d6357951cb33301f306c36276d9f4f561361
vasicev <vasicevdeki@gmail.com>
2021-05-03 11:35:06 +0200

    task3

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import home
+from django.urls import path, include
+from .views import home, TagView, TaskView
+from rest_framework import routers
+
+routers = routers.DefaultRouter()
+routers.register('tag', TagView)
+routers.register('task', TaskView)
 
 urlpatterns = [
-    path('task1/', home)
+    path('task1/', home),
+    path('task2-3/', include(routers.urls))
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Tag, Task, TagSerializer, TaskSerializer
 
 def home(request):
     return HttpResponse('Hello Wolrd!')
+
+class TagView(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class TaskView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
